d309c006400b04143c6b799aa22c606eab866ae0
Vinny Martinez <vinny@vinnymartinez.com>
2017-06-27 10:50:46 -0400

    add fix to #winner spec

--- spec/01_tic_tac_toe_spec.rb
+++ spec/01_tic_tac_toe_spec.rb
@@ -197,10 +197,10 @@ describe './lib/tic_tac_toe.rb' do
       draw?(board)
     end
 
-    it 'calls full?' do 
+    it 'calls full?' do
       board = ["X", "O", "X", "O", "X", "X", "O", "X", "O"]
 
-      expect(self).to receive(:full?).with(board)      
+      expect(self).to receive(:full?).with(board)
       draw?(board)
     end
 
@@ -251,7 +251,7 @@ describe './lib/tic_tac_toe.rb' do
     end
 
     it 'returns O when O won' do
-      board = ["X", "O", " ", " ", "O", " ", " ", "O", "X"]
+      board = ["X", "O", "X", " ", "O", " ", " ", "O", "X"]
 
       expect(winner(board)).to eq("O")
     end
